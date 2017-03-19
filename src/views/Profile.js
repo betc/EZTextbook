d2b1efcf97c10c97d863cfc9cf1c2d238fa9561a
Chenhao <chenh445@gmail.com>
2017-03-19 17:31:53 -0400

    add Profile Page

---
+++ src/views/Profile.js
@@ -0,0 +1,194 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight
+} from 'react-native';
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "John",
+      lastName: "Doe",
+      email: "johndoe@gmail.com",
+      mobile: "613-555-0151",
+      editable: false,
+      autoFocus: false,
+      update: "Update Profile"
+    };
+  }
+
+  handleChange(event) {
+    // todo
+  }
+
+  updateProfile() {
+    if (this.state.update == "Update Profile") {
+      this.setState({
+        update: "Save Change",
+        editable: true,
+        autoFocus: true
+      });
+    } else {
+      this.setState({
+        update: "Update Profile",
+        editable: false,
+        autoFocus: false
+      });
+    }
+  }
+
+  changePassword() {
+    // todo
+  }
+
+  render() {
+    return (
+      <View style={styles.containerStyle}>
+        <Image
+          source={require('../../img/profile.jpg')}
+          style={styles.imageStyle}
+        />
+        <Text style={styles.headingStyle}>
+          General Information:
+        </Text>
+        <View style={styles.cellStyle}>
+          <Text style={styles.textStyle}>
+            First Name:
+          </Text>
+          <TextInput
+            style={styles.generalInputStyle}
+            defaultValue={this.state.firstName}
+            editable={this.state.editable}
+            autoFocus={this.state.autoFocus}
+            onChangeText={this.handleChange.bind(this)}
+          />
+        </View>
+        <View style={styles.cellStyle}>
+          <Text style={styles.textStyle}>
+            Last Name:
+          </Text>
+          <TextInput
+            style={styles.generalInputStyle}
+            defaultValue={this.state.lastName}
+            editable={this.state.editable}
+            autoFocus={this.state.autoFocus}
+            onChangeText={this.handleChange.bind(this)}
+          />
+        </View>
+        <Text style={styles.headingStyle}>
+          Contact Information:
+        </Text>
+        <View style={styles.cellStyle}>
+          <Text style={styles.textStyle}>
+            Email address:
+          </Text>
+          <TextInput
+            style={styles.contactInputStyle}
+            defaultValue={this.state.email}
+            editable={this.state.editable}
+            autoFocus={this.state.autoFocus}
+            onChangeText={this.handleChange.bind(this)}
+          />
+        </View>
+        <View style={styles.cellStyle}>
+          <Text style={styles.textStyle}>
+            Mobile number:
+          </Text>
+          <TextInput
+            style={styles.contactInputStyle}
+            defaultValue={this.state.mobile}
+            editable={this.state.editable}
+            autoFocus={this.state.autoFocus}
+            onChangeText={this.handleChange.bind(this)}
+          />
+        </View>
+        <TouchableHighlight style={styles.button} onPress={this.updateProfile.bind(this)}>
+          <Text style={styles.buttonText}>
+            {this.state.update}
+          </Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={styles.button} onPress={this.changePassword.bind(this)}>
+          <Text style={styles.buttonText}>
+            Change Password
+          </Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    marginTop: 40
+  },
+  imageStyle: {
+    alignSelf: 'center',
+    height: 150,
+    width: 150,
+    borderWidth: 0.5,
+    borderRadius: 75,
+    borderColor: '#800080'
+  },
+  headingStyle: {
+    marginTop: 10,
+    color: '#550080',
+    fontSize: 20,
+    fontWeight: '700',
+    alignSelf: 'center'
+  },
+  cellStyle: {
+    alignSelf: 'center',
+    marginTop: 5,
+    flexDirection: 'row'
+  },
+  textStyle: {
+    paddingRight: 30,
+    alignSelf: 'center',
+    fontSize: 15,
+    fontWeight: '800'
+  },
+  generalInputStyle: {
+    height: 35,
+    width: 80,
+    borderColor: 'rgba(0,0,0,0)',
+    borderWidth: 2
+  },
+  contactInputStyle: {
+    height: 35,
+    width: 200,
+    borderColor: 'rgba(0,0,0,0)',
+    borderWidth: 2
+  },
+  commentInputStyle: {
+    marginTop: 10,
+    marginBottom: 10,
+    height: 100,
+    borderWidth: 2,
+    borderColor: '#550080'
+  },
+  button: {
+    height: 50,
+    width: 250,
+    backgroundColor: '#48BBEC',
+    alignSelf: 'center',
+    marginTop: 30,
+    marginLeft: 3,
+    marginRight: 3,
+    justifyContent: 'center',
+    borderRadius: 6,
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#FFF',
+    fontWeight: '500',
+    alignSelf: 'center'
+  }
+});
+
+export default Profile;
