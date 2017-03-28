6ef9a081572a587d42fcf10d7c88f1db6c90537e
Sultaan Shah <seshah@uwaterloo.ca>
2017-03-28 03:32:50 -0400

    added back button to login, edited styling

--- src/views/BarcodeScanner.js
+++ src/views/BarcodeScanner.js
@@ -3,86 +3,124 @@ import {
   AppRegistry,
   StyleSheet,
    View,
+   Text,
+   TouchableOpacity,
+   Alert
 } from 'react-native';
 import Camera from 'react-native-camera';
 import ApiUtils from '../ApiUtils';
- 
+import BookDetail from '../components/BookDetail';
+
 class BarcodeScanner extends Component {
   constructor(props) {
-    super(props);
+	super(props);
     this.state = {
-      torchMode: 'off',
-      cameraType: 'back',
-      books: [],
+	  barcode: '',
+		book: ''
     };
   }
-
-    componentWillMount() {
-        console.log("before rendering Barcode Scanner. this.props is: "+ this.props)
-        console.log("before rendering Barcode Scanner. this.props.navigator is: "+ this.props.navigator)
-
-    }
- 
-  barcodeReceived(code) {
-        console.log("this.props is: " + this.props)
-		alert("Barcode Scanned!",  "ISBN: " + code.data);
-		ApiUtils.getToken('Login_Token').then((token) => {
-			fetch('https://eztextbook.herokuapp.com/api/book/search/criteria?isbn='+ code.data +'&token='+ token)
-			.then(ApiUtils.checkStatus)
-			.then(response => response.json())
-			.then((responseJson) => {
-				console.log(responseJson);
-				if(responseJson === null) {
-
-					alert("Sorry, we couldn't find that book in our database")
-					this.props.navigator.pop(); //replace({id: 'Home'});
-				} else {
-
-					let temp = [];
-					temp.push(responseJson);
-
-					this.setState({books: temp});
-					console.log(temp);
-				}
-				this.props.navigator.push({id: 'Search' , props: {books: this.state.books, navigator: this.props.navigator}} );
-			//	this.render();
-			}).catch(error =>
-				console.log("in BookList._onBarCodeScan, error: "+error)
-			)
-		})
-  }
- 
-  render() {
-    return (
-      <View style={styles.container}>
-            <Camera
-                ref= {(cam) => {
-                    this.camera = cam}
-                }
-                style={styles.preview}
-                aspect={Camera.constants.Aspect.fill}
-                onBarCodeRead={this.barcodeReceived.bind(this)}
-                defaultTouchToFocus
-                type={Camera.constants.Type.back}
-            >
-            </Camera>
-      </View>
+  BarCodeRead(e) {
+	console.log("onBarcodeRead:")
+	console.log(e);
+	this.setState({barcode: e.data});
+	console.log(e.data);
+    Alert.alert(
+      "Barcode Found! \nData: " + e.data
     );
-  }
+  }
+
+  renderBook() {
+	ApiUtils.getToken('Login_Token').then((token) => {
+		fetch('https://eztextbook.herokuapp.com/api/book/search/criteria?isbn='+ this.state.barcode +'&token='+ token)
+		.then(ApiUtils.checkStatus)
+		.then(response => response.json())
+		.then((responseJson) => {
+			if (responseJson.success !== false) {
+				this.setState({
+				  book: responseJson
+				})
+			}
+		})
+		.catch(error =>
+				 console.log("in BookList._onBarCodeScan, error: "+error)
+		)
+	});
+	if (this.state.book === '') {
+	    return (
+	        <View style={styles.scanButton}>
+	            <TouchableOpacity
+	                onPress={this.scanAgain.bind(this)}
+	                style={styles.afterScan}>
+	                <Text>Book Not Found. Press Here To Scan Another Barcode</Text>
+	            </TouchableOpacity>
+	        </View>
+        )
+	} else {
+	  return <BookDetail book={this.state.book} navigator={this.props.navigator}/>
+	}
+  }
+
+  scanAgain() {
+	this.setState({barcode: ''});
+  }
+
+  renderBarcode() {
+	if (this.state.barcode == '') {
+	  return (
+		<Camera
+			ref="cam"
+			style={styles.container}
+			onBarCodeRead={this.BarCodeRead.bind(this)}
+			type={Camera.constants.Type.back}>
+		</Camera>
+	  )
+	} else {
+	  return (
+	  <View>
+		{this.renderBook()}
+	  </View>
+	  )
+	}
+  }
+
+  render() {
+	return (
+		this.renderBarcode()
+	);
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
+	flex: 1,
+	flexDirection: 'row',
+	justifyContent: "center",
+	alignItems: "center",
+	backgroundColor: "transparent",
   },
   preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+	flex: 1,
+	justifyContent: 'flex-end',
+	alignItems: 'center'
+  },
+  afterScan: {
+    alignSelf: 'center',
+    fontSize: 20,
+    color: '#FFF',
+  },
+  scanButton: {
+    height: 50,
+    width: 300,
+    backgroundColor: '#48BBEC',
+    alignSelf: 'center',
+    marginTop: 30,
+    marginLeft: 3,
+    marginRight: 3,
+    justifyContent: 'center',
+    borderRadius: 6,
   }
 
 });
 
 export default BarcodeScanner;
-
